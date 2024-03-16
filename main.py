a4af72854872c8bf1970b617b657e85216436641
Anonymous <124679005+PivoEnjoyer62@users.noreply.github.com>
2024-03-16 19:35:57 +0200

    lesson2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 from PIL import *
 from PIL import Image
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
 app = QApplication([])
 
@@ -34,7 +37,20 @@ app.setStyleSheet("""
 
 
 
-
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
 
 
 
@@ -76,9 +92,41 @@ V2 = QVBoxLayout()
 H1.addLayout(V1)
 H1.addLayout(V2)
 
+class WorkWithPhoto:
+    def __init__(self):
+        self.image = None
+        self.Folder = None
+        self.image_name = None
+    def show_image(self):
+        pass
+
+    def load(self):
+        full_path = os.path.join(self.folder, self.image_name)
+        self.image = Image.open(full_path)
 
 
+    def show_image(self):
+        pixel = pil2pixmap(self.image)
+        photo.setPixmap(pixel)
+work_with_photo = WorkWithPhoto()
 
+def show_directory():
+    work_with_photo.folder = QFileDialog.getExistingDirectory()
+    list_files= os.listdir(work_with_photo.folder)
+
+    for file in list_files:
+        if file.endswith("png"):
+            info_list.addItem(file)
+
+def show_photo():
+    image_name = info_list.currentItem().text()
+    print(image_name)
+    work_with_photo.image_name = image_name
+    work_with_photo.load()
+    work_with_photo.show_image()
+
+info_list.currentRowChanged.connect(show_photo)
+open_papk.clicked.connect(show_directory)
 
 
 
@@ -92,6 +140,7 @@ H2.addWidget(filter_2)
 H2.addWidget(filter_3)
 H2.addWidget(filter_4)
 H2.addWidget(filter_5)
+
 open_papk.setObjectName("papk")
 window.setLayout(H1)
 
