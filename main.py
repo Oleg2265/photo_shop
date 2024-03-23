30bba9c4032151097e9f9a518e62b1958c0db5e7
Anonymous <124679005+PivoEnjoyer62@users.noreply.github.com>
2024-03-23 19:21:14 +0200

    lesson_end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+from time import sleep
 
 from PIL import *
-from PIL import Image
+from PIL import Image, ImageFilter
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
 app = QApplication([])
@@ -20,6 +21,13 @@ app.setStyleSheet("""
             padding: 12px;
         }
 
+        QPushButton#lamat {
+            color:#00000;
+            background-color: #FF0A0E;
+            border-color:#B30B0E;
+            border-width: 9px;
+            padding: 12px;
+        }
         QPushButton
         {
             background-color: #ffb600;
@@ -61,6 +69,7 @@ def pil2pixmap(im):
 
 window = QWidget()
 window.resize(700,500)
+
 app.setStyle('Windows')
 
 
@@ -82,6 +91,12 @@ filter_2 = QPushButton("Фільтер 2")
 filter_3 = QPushButton("Фільтер 3")
 filter_4 = QPushButton("Фільтер 4")
 filter_5 = QPushButton("Фільтер 5")
+filter_6 = QPushButton("Фільтер 6")
+filter_7 = QPushButton("Фільтер 7")
+knopka_yzhas = QPushButton("НЕ НАЖИМАЙ!")
+destroy_button = QPushButton("взламати программе")
+
+
 
 H1 = QHBoxLayout()
 H2 = QHBoxLayout()
@@ -108,7 +123,49 @@ class WorkWithPhoto:
     def show_image(self):
         pixel = pil2pixmap(self.image)
         photo.setPixmap(pixel)
+
+
+    def rozmit(self):
+        self.image = self.image.filter(ImageFilter.BLUR)
+        self.show_image()
+
+    def emo_kaktus(self):
+        self.image = self.image.filter(ImageFilter.EMBOSS)
+        self.show_image()
+
+    def contor(self):
+        self.image = self.image.filter(ImageFilter.CONTOUR)
+        self.show_image()
+
+    def smmothj(self):
+        self.image = self.image.filter(ImageFilter.SMOOTH)
+        self.show_image()
+
+    def DETALNIST(self):
+        self.image = self.image.filter(ImageFilter.DETAIL)
+        self.show_image()
+
+    def open_ima(self):
+        self.image = Image.open("Мега Секретне Фото.png")
+        self.show_image()
+    def enchance(self):
+        self.image = self.image.filter(ImageFilter.EDGE_ENHANCE)
+        self.show_image()
+
+    def KVADRAT(self):
+        self.image = self.image.filter(ImageFilter.BoxBlur)
+        self.show_image()
 work_with_photo = WorkWithPhoto()
+
+
+def mega_destroy():
+    window.hide()
+    for i in range(15):
+        print("твоя программа вернеться через 4 секунди!")
+    sleep(4)
+    window.show()
+
+
 
 def show_directory():
     work_with_photo.folder = QFileDialog.getExistingDirectory()
@@ -117,6 +174,14 @@ def show_directory():
     for file in list_files:
         if file.endswith("png"):
             info_list.addItem(file)
+
+
+def knopka_yzhasa():
+    image_name = ""
+    print(image_name)
+
+    image_name.load()
+    image_name.show_image()
 
 def show_photo():
     image_name = info_list.currentItem().text()
@@ -127,9 +192,15 @@ def show_photo():
 
 info_list.currentRowChanged.connect(show_photo)
 open_papk.clicked.connect(show_directory)
-
-
-
+filter_1.clicked.connect(work_with_photo.rozmit)
+filter_2.clicked.connect(work_with_photo.emo_kaktus)
+filter_3.clicked.connect(work_with_photo.contor)
+filter_4.clicked.connect(work_with_photo.smmothj)
+filter_5.clicked.connect(work_with_photo.DETALNIST)
+knopka_yzhas.clicked.connect(work_with_photo.open_ima)
+filter_6.clicked.connect(work_with_photo.enchance)
+filter_7.clicked.connect(work_with_photo.KVADRAT)
+destroy_button.clicked.connect(mega_destroy)
 V1.addWidget(open_papk)
 V1.addWidget(info_list)
 
@@ -140,8 +211,12 @@ H2.addWidget(filter_2)
 H2.addWidget(filter_3)
 H2.addWidget(filter_4)
 H2.addWidget(filter_5)
-
+H2.addWidget(filter_6)
+H2.addWidget(filter_7)
+V2.addWidget(destroy_button)
+H2.addWidget(knopka_yzhas)
 open_papk.setObjectName("papk")
+destroy_button.setObjectName("lamat")
 window.setLayout(H1)
 
 
